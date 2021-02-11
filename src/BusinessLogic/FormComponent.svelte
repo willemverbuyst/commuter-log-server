db95e600e1a331555965e5a6c554c39f5fee727d
Willem Verbuyst <willemverbuyst@gmail.com>
2021-02-11 07:42:17 +0100

    Add datepicker

--- src/BusinessLogic/FormComponent.svelte
+++ src/BusinessLogic/FormComponent.svelte
@@ -1,7 +1,10 @@
 <script>
   import InputComponent from '../UI/InputComponent.svelte';
+  import Datepicker from 'svelte-calendar';
 
   let title = '';
+  let minDate = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
+  let maxDate = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
 </script>
 
 <form>
@@ -11,6 +14,7 @@
     value={title}
     on:input={(event) => (title = event.target.value)}
   />
+  <Datepicker start={minDate} end={maxDate} />
 </form>
 
 <style>
