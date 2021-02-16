3701afa70c500f4099462a20079aec83002fe5cd
Willem Verbuyst <willemverbuyst@gmail.com>
2021-02-16 19:03:08 +0100

    Customize barchart

--- src/UI/Chart.svelte
+++ src/UI/Chart.svelte
@@ -1,50 +1,81 @@
 <script>
   import Bar from 'svelte-chartjs/src/Bar.svelte';
+  import 'chartjs-plugin-datalabels';
+
+  let travelTimes = [300, 70, 70, 70, 130, 130];
+  const max = Math.max(...travelTimes) * 1.2;
+
+  function formatDataLabels(value) {
+    if (value % 60 === 0) {
+      return `0${Math.floor(value / 60)}:00`;
+    }
+    return `0${Math.floor(value / 60)}:${value % 60}`;
+  }
 
   let data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
     datasets: [
       {
-        label: '% of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        data: travelTimes,
         backgroundColor: [
           '#e20074',
           '#e20074',
           '#e20074',
           '#e20074',
           '#e20074',
-          '#e20074',
         ],
+        borderWidth: 0,
+        barPercentage: 1,
       },
     ],
   };
 
   let options = {
-    maintainAspectRatio: false,
+    maintainAspectRatio: true,
+    legend: {
+      display: false,
+    },
     responsive: true,
     scales: {
       xAxes: [
         {
-          barPercentage: 1,
           gridLines: {
-            display: true,
-            color: 'rgba(0, 0, 0, 0.1)',
+            display: false,
           },
         },
       ],
       yAxes: [
         {
           gridLines: {
-            display: true,
-            color: 'rgba(0, 0, 0, 0.1)',
+            display: false,
           },
           ticks: {
+            display: false,
             beginAtZero: true,
+            suggestedMax: max,
           },
         },
       ],
     },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'top',
+        display: true,
+        color: 'white',
+        formatter: (value) => formatDataLabels(value),
+      },
+    },
   };
 </script>
 
-<Bar {data} {options} width={100} height={50} />
+<div class="chart-container">
+  <Bar {data} {options} />
+</div>
+
+<style>
+  .chart-container {
+    width: 600px;
+    margin: 4rem auto;
+  }
+</style>
