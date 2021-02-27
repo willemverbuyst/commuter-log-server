7d5a8dca9ef71621c260e103cb52d75611efc3ff
Willem Verbuyst <willemverbuyst@gmail.com>
2021-02-27 06:29:04 +0100

    Add gradient to line chart

--- src/UI/Charts/AllWorkingDays.svelte
+++ src/UI/Charts/AllWorkingDays.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Line from 'svelte-chartjs/src/Line.svelte';
+  import { onMount } from 'svelte';
   import 'chartjs-plugin-datalabels';
   import { workingDays } from '../../dummyData';
   import {
@@ -9,96 +9,115 @@
 
   const {
     travelTimes,
-    backgroundColor,
     maxForDisplay,
     labels,
     title,
     lineValue,
   } = getAllWorkingDaysData(workingDays);
 
-  let data = {
-    labels,
-    datasets: [
-      {
-        label: 'Line Dataset',
-        data: lineValue,
-        borderJoinStyle: 'miter',
-        type: 'line',
-        fill: false,
-        borderColor: 'purple',
-        borderWidth: 2,
-        // this dataset is drawn on top
-        order: 1,
-      },
-      {
-        data: travelTimes,
-        backgroundColor: 'rgba(255, 159, 64, 0.8)',
-        fill: true,
-        lineTension: 0.3,
-        borderColor: 'rgba(255, 159, 64, 1)',
-        borderWidth: 3,
-        barPercentage: 1,
-        order: 2,
-      },
-    ],
-  };
+  function createChart() {
+    const ctx = document.getElementById('myChart').getContext('2d');
 
-  let options = {
-    title: {
-      display: true,
-      text: title,
-    },
-    maintainAspectRatio: true,
-    legend: {
-      display: false,
-    },
-    elements: {
-      point: {
-        radius: 0,
-      },
-    },
-    responsive: true,
-    scales: {
-      xAxes: [
-        {
-          gridLines: {
-            display: false,
+    const gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+    gradientStroke.addColorStop(0, 'rgba(255, 159, 64, 0.8)');
+    gradientStroke.addColorStop(0.5, 'rgba(255, 99, 132, 0.8)');
+    gradientStroke.addColorStop(1, 'rgba(255, 39, 204, 0.8)');
+
+    var gradientFill = ctx.createLinearGradient(500, 0, 100, 0);
+    gradientFill.addColorStop(0, 'rgba(255, 159, 64, 0.8)');
+    gradientFill.addColorStop(0.5, 'rgba(255, 99, 132, 0.8)');
+    gradientFill.addColorStop(1, 'rgba(255, 39, 204, 0.8)');
+
+    const myChart = new Chart(ctx, {
+      type: 'line',
+
+      data: {
+        labels,
+        datasets: [
+          {
+            label: 'Line Dataset',
+            data: lineValue,
+            borderJoinStyle: 'miter',
+            type: 'line',
+            fill: false,
+            borderColor: 'purple',
+            borderWidth: 2,
+            order: 1,
           },
-          ticks: {
-            display: false,
+          {
+            data: travelTimes,
+            borderColor: gradientStroke,
+            fill: true,
+            lineTension: 0.3,
+            backgroundColor: gradientFill,
+            borderWidth: 2,
+            order: 2,
+          },
+        ],
+      },
+      options: {
+        animation: {
+          easing: 'easeInOutBack',
+        },
+        title: {
+          display: true,
+          text: title,
+        },
+        maintainAspectRatio: true,
+        legend: {
+          display: false,
+        },
+        elements: {
+          point: {
+            radius: 0,
           },
         },
-      ],
-      yAxes: [
-        {
-          gridLines: {
+        responsive: true,
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+              ticks: {
+                display: false,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+              ticks: {
+                display: false,
+                beginAtZero: true,
+                suggestedMax: maxForDisplay,
+              },
+            },
+          ],
+        },
+        tooltips: {
+          enabled: false,
+        },
+        plugins: {
+          datalabels: {
+            anchor: 'end',
+            align: 'top',
             display: false,
-          },
-          ticks: {
-            display: false,
-            beginAtZero: true,
-            suggestedMax: maxForDisplay,
+            color: '#170a3a',
+            formatter: (value) => formatDataLabels(value),
           },
         },
-      ],
-    },
-    tooltips: {
-      enabled: false,
-    },
-    plugins: {
-      datalabels: {
-        anchor: 'end',
-        align: 'top',
-        display: false,
-        color: '#170a3a',
-        formatter: (value) => formatDataLabels(value),
       },
-    },
-  };
+    });
+  }
+
+  onMount(createChart);
 </script>
 
 <div class="chart-container">
-  <Line {data} {options} />
+  <canvas id="myChart" />
 </div>
 
 <style>
