9b910dc6c5f660d489269183fd9026b0988c5f8d
Willem Verbuyst <willemverbuyst@gmail.com>
2021-03-10 06:05:45 +0100

    Make holes in modal

--- src/UI/Inputs/Dropdown.svelte
+++ src/UI/Inputs/Dropdown.svelte
@@ -3,7 +3,8 @@
   export let routes;
 </script>
 
-<div class="form-control">
+<div class="time-input__container">
+  <div class="time-input__label">Trip One</div>
   <!-- svelte-ignore a11y-no-onchange -->
   <select value={route} on:change>
     {#each routes as r}
@@ -13,14 +14,38 @@
 </div>
 
 <style>
+  .time-input__container {
+    width: 200px;
+    margin: 20px auto;
+    /* border: 1px solid #aaa;
+    border-radius: 7px; */
+    border: 2px solid #333;
+    border-radius: 7px;
+    box-shadow: inset 2px 2px 2px #222, inset -2px -2px 2px #444;
+    padding: 10px 0;
+    position: relative;
+  }
+
+  .time-input__label {
+    text-align: left;
+    padding: 0 0.5rem;
+    position: absolute;
+    top: -10px;
+    left: 10px;
+    background-color: #333;
+    /* z-index: -200; */
+    /* background-color: rgba(0, 0, 0, 0.3); */
+    /* backdrop-filter: blur(10px) saturate(100%) contrast(45%) brightness(130%); */
+  }
+
   select {
-    width: 340px;
+    width: 90%;
+    padding: 0 0.5rem 0 0;
+    margin: 0.5rem 0 0 0;
     font: inherit;
     color: inherit;
-    border: 1px solid #170a3a;
-    border-radius: 7px;
+    border: none;
     background: transparent;
-    padding: 10px 0;
     transition: border-color 0.1s ease-out;
     cursor: pointer;
   }
@@ -34,8 +59,4 @@
     margin-bottom: 0.5rem;
     width: 100%;
   } */
-
-  .form-control {
-    width: 100%;
-  }
 </style>
--- src/UI/Modal/Modal.svelte
+++ src/UI/Modal/Modal.svelte
@@ -12,21 +12,40 @@
 </script>
 
 <div class="modal-backdrop" on:click={closeModal} />
-<div
-  class="modal"
-  in:scale={{ duration: 400, easing: cubicIn }}
-  out:fly={{ y: 300 }}
->
-  <div class="content">
-    <h1>Add a day to log</h1>
-    <slot />
+<div>
+  <div class="modal-left">
+    <div
+      class="modal-left--top"
+      in:scale={{ duration: 400, easing: cubicIn }}
+      out:fly={{ y: 300 }}
+    />
+    <div
+      class="modal-left--bottom"
+      in:scale={{ duration: 400, easing: cubicIn }}
+      out:fly={{ y: 300 }}
+    />
   </div>
-  <footer>
-    <slot name="footer">
-      <Button on:click={closeModal}>Close</Button>
-      <Button on:click={closeModal}>Add</Button>
-    </slot>
-  </footer>
+  <div
+    class="modal"
+    in:scale={{ duration: 400, easing: cubicIn }}
+    out:fly={{ y: 300 }}
+  >
+    <div class="content">
+      <h1>Add a day to log</h1>
+      <slot />
+    </div>
+    <footer>
+      <slot name="footer">
+        <Button on:click={closeModal}>Close</Button>
+        <Button on:click={closeModal}>Add</Button>
+      </slot>
+    </footer>
+  </div>
+  <div
+    class="modal-right"
+    in:scale={{ duration: 400, easing: cubicIn }}
+    out:fly={{ y: 300 }}
+  />
 </div>
 
 <style>
@@ -38,9 +57,51 @@
     height: 100%;
   }
 
-  .modal:before {
+  .modal-left--top {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 20vw;
+    min-height: 50vh;
+    background: -webkit-radial-gradient(
+      65% 60%,
+      circle,
+      transparent 30%,
+      #222 30%
+    );
+  }
+  .modal-left--bottom {
+    position: fixed;
+    top: 50vh;
+    left: 0;
+    width: 20vw;
+    min-height: 50vh;
+    background: -webkit-radial-gradient(
+      75% 80%,
+      circle,
+      transparent 15%,
+      #222 15%
+    );
+  }
+
+  .modal-right {
+    position: fixed;
+    top: 0;
+    right: 0;
+    width: 20vw;
+    min-height: 100vh;
+    background: -webkit-radial-gradient(
+      35% 70%,
+      circle,
+      transparent 15%,
+      #222 15%
+    );
+  }
+
+  /* .modal:before {
     background-color: rgba(0, 0, 0, 0.3);
     backdrop-filter: blur(10px) saturate(100%) contrast(45%) brightness(130%);
+    background-color: #333;
     content: '';
     height: 100%;
     position: absolute;
@@ -53,17 +114,20 @@
     -webkit-box-shadow: 5px 3px 5px 0px rgba(0, 0, 0, 0.25);
     -moz-box-shadow: 5px 3px 5px 0px rgba(0, 0, 0, 0.25);
     z-index: -100;
-  }
+  } */
 
   .modal {
     position: fixed;
-    top: 5vh;
-    left: 50%;
-    transform: translateX(-50%);
+    top: 0;
+    left: 20vw;
     width: 60vw;
-    min-height: 80vh;
-    z-index: 3000;
-    border-radius: 7px;
+    min-height: 100vh;
+    background: -webkit-radial-gradient(
+      90% 20%,
+      circle,
+      transparent 5%,
+      #222 5%
+    );
   }
 
   .content {
