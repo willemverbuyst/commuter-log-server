1ce124c93a92c69208381334230dc0e14602992f
Willem Verbuyst <willemverbuyst@gmail.com>
2021-04-03 08:21:47 +0200

    Update CarVsPublicChart to TS

--- src/helpers/chartLogic/carVsPublicChart.ts
+++ src/helpers/chartLogic/carVsPublicChart.ts
@@ -1,8 +1,22 @@
+import type { LogDate } from '../../models/Logdata';
 import { reduceDates } from '../utils';
 
 // DOUGHNUT CHART, car vs public chart
 // Display total travel times public transport vs total travel times car
-export const getCarVsPublicTotalsData = (logData) => {
+export const getCarVsPublicTotalsData = (
+  logData: LogDate[]
+): {
+  totalsCar: {
+    numberOfDaystravelled: number;
+    totalTimeTravelled: number;
+  };
+  totalsPublic: {
+    numberOfDaystravelled: number;
+    totalTimeTravelled: number;
+  };
+  labels: string[];
+  title: string;
+} => {
   // Combine all the days with the same date
   const reducedDates = reduceDates(logData);
 
@@ -19,8 +33,15 @@ export const getCarVsPublicTotalsData = (logData) => {
   };
 };
 
+// TO DO: FIX DURATION TRIP (!)
 // Get the total of all working days at the office for car or for public transport
-const getTotalsTransport = (workingDays, transport) => {
+const getTotalsTransport = (
+  workingDays: LogDate[],
+  transport: string
+): {
+  numberOfDaystravelled: number;
+  totalTimeTravelled: number;
+} => {
   const daysTravelled = workingDays.filter(
     (day) =>
       day.statusOfDay === 'working at the office' &&
@@ -29,7 +50,7 @@ const getTotalsTransport = (workingDays, transport) => {
   const numberOfDaystravelled = daysTravelled.length;
   const totalTimeTravelled =
     daysTravelled.length > 0
-      ? daysTravelled.map((day) => day.durationTrip).reduce((a, b) => a + b)
+      ? daysTravelled.map((day) => day.durationTrip!).reduce((a, b) => a + b)
       : 0;
 
   return {
