a190e8e33591392965922bb26cd632d1cbcbd1dd
Willem Verbuyst <willemverbuyst@gmail.com>
2021-04-03 05:58:05 +0200

    Set up model for logdate

---
+++ src/models/Logdata.ts
@@ -0,0 +1,14 @@
+export type LogDate = {
+  id?: string;
+  date: Date;
+  durationTrip?: number;
+  meansOfTransport?: MeansOfTransport;
+  routeTripFrom?: string;
+  routeTripTo?: string;
+  statusOfDay: StatusOfDay;
+  weekNumber: number;
+};
+
+type MeansOfTransport = 'car' | 'public transport';
+
+type StatusOfDay = 'day off' | 'working at the office' | 'working from home';
