cb4cc93690f0dbc3545c9f228bf43794d2f67e40
Willem Verbuyst <willemverbuyst@gmail.com>
2021-04-04 14:47:05 +0200

    Test getMinutes

--- src/helpers/chartLogic/chartLogic.test.ts
+++ src/helpers/chartLogic/chartLogic.test.ts
@@ -3,7 +3,7 @@ import {
   colorTravelByCar,
   colorTravelByPublicTransport,
 } from '../../UI/colors';
-import { getBackgroundColor } from './chartLogic';
+import { getBackgroundColor, getMinutes } from './chartLogic';
 
 const testLogDateOne: LogDate = {
   id: 'test_id',
@@ -19,11 +19,22 @@ const testLogDateOne: LogDate = {
 const testLogDateTwo: LogDate = {
   id: 'test_id',
   date: new Date(),
-  durationTrip: 90,
+  durationTrip: 60,
   meansOfTransport: 'public transport',
   routeTripFrom: 'test_start',
   routeTripTo: 'test_destination',
   statusOfDay: 'working at the office',
+  weekNumber: 1,
+};
+
+const testLogDateThree: LogDate = {
+  id: 'test_id',
+  date: new Date(),
+  durationTrip: 0,
+  meansOfTransport: '',
+  routeTripFrom: '',
+  routeTripTo: '',
+  statusOfDay: 'working from home',
   weekNumber: 1,
 };
 
@@ -37,5 +48,18 @@ describe('if getBackgroundColor is given an array of logDates', () => {
       colorTravelByCar,
       colorTravelByPublicTransport,
     ]);
+  });
+});
+
+describe('if getMinutes is given an array of logDates', () => {
+  test('returns a the array with minutes', () => {
+    expect(getMinutes([testLogDateOne])).toEqual([90]);
+    expect(getMinutes([testLogDateTwo])).toEqual([60]);
+    expect(getMinutes([testLogDateThree])).toEqual([0.00001]);
+    expect(getMinutes([testLogDateThree])).not.toEqual([0.0001]);
+    expect(getMinutes([testLogDateOne, testLogDateTwo])).toEqual([90, 60]);
+    expect(
+      getMinutes([testLogDateOne, testLogDateTwo, testLogDateThree])
+    ).toEqual([90, 60, 0.00001]);
   });
 });
