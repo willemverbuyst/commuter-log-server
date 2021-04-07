a59edffd7a5d8ae25ab1ab4e72f2aa428a44e05b
Willem Verbuyst <willemverbuyst@gmail.com>
2021-04-07 19:07:18 +0200

    Test utils

--- src/helpers/utils.test.ts
+++ src/helpers/utils.test.ts
@@ -1,8 +1,24 @@
 import type { LogDate } from '../models/Logdata';
-import { chunkArray } from './utils';
+import {
+  chunkArray,
+  getDay,
+  getUniqueValues,
+  getYear,
+  reduceDates,
+} from './utils';
 
 const logDate: LogDate = {
-  date: new Date(),
+  date: new Date('1/1/2021'),
+  durationTrip: 30,
+  meansOfTransport: 'car',
+  routeTripFrom: '',
+  routeTripTo: '',
+  statusOfDay: 'working from home',
+  weekNumber: 1,
+};
+
+const logDateTwo: LogDate = {
+  date: new Date('1/2/2021'),
   durationTrip: 0,
   meansOfTransport: 'car',
   routeTripFrom: '',
@@ -11,12 +27,61 @@ const logDate: LogDate = {
   weekNumber: 1,
 };
 
-const logDateTwo = [logDate, logDate];
+const logDateReduced = {
+  date: new Date('1/1/2021'),
+  durationTrip: 60,
+  meansOfTransport: 'car',
+  routeTripFrom: '',
+  routeTripTo: '',
+  statusOfDay: 'working from home',
+  weekNumber: 1,
+};
+
+const logDateThree = [logDate, logDate];
 const logDateFour = [logDate, logDate, logDate, logDate];
 
 describe('if chunkArray is given an array', () => {
   test('returns a chunked array', () => {
-    expect(chunkArray(logDateTwo, 1)).toEqual([[logDate], [logDate]]);
-    expect(chunkArray(logDateFour, 2)).toEqual([logDateTwo, logDateTwo]);
+    expect(chunkArray(logDateThree, 1)).toEqual([[logDate], [logDate]]);
+    expect(chunkArray(logDateFour, 2)).toEqual([logDateThree, logDateThree]);
+  });
+});
+
+describe('if getDay is given a date', () => {
+  test('returns a string', () => {
+    const testDay = new Date('1/1/2021');
+    expect(getDay(testDay)).toBe('Fri Jan 01');
+  });
+});
+
+describe('if getUniqueValues is given an value, index and array', () => {
+  test('returns a boolean', () => {
+    expect(getUniqueValues('x', 0, ['x', 'y', 'z'])).toBe(true);
+    expect(getUniqueValues('x', 1, ['x', 'x', 'y', 'z'])).toBe(false);
+    expect(getUniqueValues('y', 2, ['x', 'x', 'y', 'z'])).toBe(true);
+    expect(getUniqueValues('y', 3, ['y', 'x', 'x', 'y', 'z'])).toBe(false);
+  });
+});
+
+describe('if getYear is given a date', () => {
+  test('returns a string', () => {
+    const testDay = new Date('1/1/2021');
+    expect(getYear(testDay)).toBe('2021');
+  });
+});
+
+describe('if reduceDates is given an array', () => {
+  test('returns a reduces array', () => {
+    expect(reduceDates([logDate, logDate, logDateTwo])).toEqual([
+      logDateReduced,
+      logDateTwo,
+    ]);
+  });
+  test('returns a reduces array', () => {
+    expect(reduceDates([logDate, logDate, logDateTwo])).not.toEqual([
+      logDate,
+      logDate,
+      logDateTwo,
+    ]);
   });
 });
