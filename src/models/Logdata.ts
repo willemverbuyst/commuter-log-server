710cecc4d70eb3bd083a811ea97ef478addbab70
Willem Verbuyst <willemverbuyst@gmail.com>
2021-04-08 20:24:19 +0200

    Create interfaces

--- src/models/Logdata.ts
+++ src/models/Logdata.ts
@@ -1,13 +1,37 @@
-export type LogDate = {
+// export interface LogDate {
+//   id?: string;
+//   date: Date;
+//   durationTrip: number;
+//   meansOfTransport: MeansOfTransport;
+//   routeTripFrom: string;
+//   routeTripTo: string;
+//   statusOfDay: StatusOfDay;
+//   weekNumber: number;
+// }
+
+interface Day {
   id?: string;
   date: Date;
+  weekNumber: number;
+}
+
+interface DayOff extends Day {
+  statusOfDay: 'day off';
+}
+
+interface WorkingFromHome extends Day {
+  statusOfDay: 'working from home';
+}
+
+interface WorkingAtTheOffice extends Day {
   durationTrip: number;
   meansOfTransport: MeansOfTransport;
   routeTripFrom: string;
   routeTripTo: string;
-  statusOfDay: StatusOfDay;
-  weekNumber: number;
-};
+  statusOfDay: 'working at the office';
+}
+
+export type LogDate = DayOff | WorkingFromHome | WorkingAtTheOffice;
 
 export type MeansOfTransport = 'car' | 'public transport' | '';
 
