928a187d09ecdfe8d36e2f0136a6a5c9b75b627e
Willem Verbuyst <willemverbuyst@gmail.com>
2021-09-16 19:23:54 +0200

    Section 4 user use cases

---
+++ tests/unit/useCases/users.test.js
@@ -0,0 +1,132 @@
+const Chance = require('chance');
+const { v4: uuidv4 } = require('uuid');
+const {
+  user: {
+    addUserUseCase,
+    deleteUserUseCase,
+    getUserByIdUseCase,
+    updateUserUseCase,
+  },
+} = require('../../../src/useCases');
+const {
+  constants: {
+    userConstants: { genders },
+  },
+} = require('../../../src/entities');
+
+const chance = new Chance();
+
+describe('User use cases', () => {
+  const mockUserRepo = {
+    add: jest.fn(async (user) => ({
+      ...user,
+      id: uuidv4(),
+    })),
+    getById: jest.fn(async (id) => ({
+      id,
+      name: chance.name(),
+      lastName: chance.last(),
+      gender: genders.MALE,
+      meta: {},
+    })),
+    update: jest.fn(async (user) => user),
+    delete: jest.fn(async (user) => user),
+  };
+
+  const depencies = {
+    usersRepository: mockUserRepo,
+  };
+
+  describe('Add user use case', () => {
+    test('User shoud be added', async () => {
+      // create user data
+      const testUserData = {
+        name: chance.name(),
+        lastName: chance.last(),
+        gender: genders.FEMALE,
+        meta: { hair: { color: chance.color() } },
+      };
+      // add a user using the use case
+      const addedUser = await addUserUseCase(depencies).execute(testUserData);
+      // check the received data
+      expect(addedUser).toBeDefined();
+      expect(addedUser.id).toBeDefined();
+      expect(addedUser.name).toBe(testUserData.name);
+      expect(addedUser.lastName).toBe(testUserData.lastName);
+      expect(addedUser.gender).toBe(testUserData.gender);
+      expect(addedUser.meta).toEqual(testUserData.meta);
+      // check that the depencies are called as expected
+      const call = mockUserRepo.add.mock.calls[0][0];
+      expect(call.id).toBeUndefined();
+      expect(call.name).toBe(testUserData.name);
+      expect(call.lastName).toBe(testUserData.lastName);
+      expect(call.gender).toBe(testUserData.gender);
+      expect(call.meta).toEqual(testUserData.meta);
+    });
+  });
+
+  describe('GetById user use case', () => {
+    test('User should be returned by id', async () => {
+      // generate fake id
+      const fakeId = uuidv4();
+      // call get user by id
+      const userById = await getUserByIdUseCase(depencies).execute({
+        id: fakeId,
+      });
+      // check the data
+      expect(userById).toBeDefined();
+      expect(userById.id).toBe(fakeId);
+      expect(userById.name).toBeDefined();
+      expect(userById.lastName).toBeDefined();
+      expect(userById.gender).toBeDefined();
+      expect(userById.meta).toBeDefined();
+      // check the call
+      const expectedId = mockUserRepo.getById.mock.calls[0][0];
+      expect(expectedId).toBe(fakeId);
+    });
+  });
+
+  describe('Update user use case', () => {
+    test('User should be updated', async () => {
+      // create user
+      const testData = {
+        id: uuidv4(),
+        name: chance.name(),
+        lastName: chance.last(),
+        gender: genders.FEMALE,
+        meta: { hair: { color: chance.color() } },
+      };
+      const updatedUser = await updateUserUseCase(depencies).execute({
+        user: testData,
+      });
+      // check the result
+      expect(updatedUser).toEqual(testData);
+
+      // check the call
+      const expectedUser = mockUserRepo.update.mock.calls[0][0];
+      expect(expectedUser).toEqual(testData);
+    });
+  });
+
+  describe('Delete user use case', () => {
+    test('User should be deleted', async () => {
+      // create user
+      const testData = {
+        id: uuidv4(),
+        name: chance.name(),
+        lastName: chance.last(),
+        gender: genders.FEMALE,
+        meta: { hair: { color: chance.color() } },
+      };
+      const deletedUser = await deleteUserUseCase(depencies).execute({
+        user: testData,
+      });
+      // check the result
+      expect(deletedUser).toEqual(testData);
+
+      // check the call
+      const expectedUser = mockUserRepo.delete.mock.calls[0][0];
+      expect(expectedUser).toEqual(testData);
+    });
+  });
+});
