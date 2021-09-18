63b3bb732cf6f0ee8b8c6ac17dcd0e7893553098
Willem Verbuyst <willemverbuyst@gmail.com>
2021-09-18 11:19:14 +0200

    Add error handling

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ const API_PREFIX = process.env.API_PREFIX || '/api/v1';
 const dependencies = require('./config/dependencies');
 
 const PORT = process.env.PORT || 4000;
+
+const ErrorHandler = require('./frameworks/expressSpecific/ErrorHandler');
+
 module.exports = {
   start: () => {
     // middlewares
@@ -20,6 +23,8 @@ module.exports = {
     );
     // routes
     app.use(API_PREFIX, routes(dependencies));
+
+    app.use(ErrorHandler);
 
     // common error handler
     app.listen(PORT, () => {
