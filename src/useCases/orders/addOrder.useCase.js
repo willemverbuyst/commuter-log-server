0a7b680ab9084a1e1cffc8b8bc6b1cf1ab24365a
Willem Verbuyst <willemverbuyst@gmail.com>
2021-09-20 19:52:32 +0200

    Separate validators

--- src/useCases/orders/addOrder.useCase.js
+++ src/useCases/orders/addOrder.useCase.js
@@ -1,69 +1,18 @@
 const { isEmpty } = require('lodash');
 const { Order } = require('../../entities');
-const { ResponseError, ValidationError } = require('../../frameworks/common');
+
+const { ResponseError } = require('../../frameworks/common');
+
+const validator = require('./validator');
 
 module.exports = (dependencies) => {
-  const {
-    ordersRepository,
-    useCases: {
-      user: { getUserByIdUseCase },
-      product: { getProductByIdUseCase },
-    },
-  } = dependencies;
+  const { ordersRepository } = dependencies;
 
   if (!ordersRepository) {
-    throw new Error('The orders repository should exist in dependencies');
+    throw new Error('ordersRepository should be exist in dependencies');
   }
 
-  if (!getUserByIdUseCase) {
-    throw new Error('getUserByIdUseCase should exist in dependencies');
-  }
-
-  if (!getProductByIdUseCase) {
-    throw new Error('getProductByIdUseCase should exist in dependencies');
-  }
-
-  const getUserById = getUserByIdUseCase(dependencies).execute;
-  const getProductById = getProductByIdUseCase(dependencies).execute;
-
-  const getValidationErrors = async ({ order }) => {
-    const returnable = [];
-
-    const { productIds = [], userId } = order;
-
-    const products = await Promise.all(
-      productIds.map((id) => getProductById({ id })),
-    );
-
-    const notFoundIds = products.reduce((acc, product, index) => {
-      if (!product) {
-        acc.push(productIds[index]);
-      }
-      return acc;
-    }, []);
-
-    if (!isEmpty(notFoundIds)) {
-      returnable.push(
-        new ValidationError({
-          field: 'productIds',
-          msg: `No products with ids ${notFoundIds.join(', ')}`,
-        }),
-      );
-    }
-
-    const user = await getUserById({ id: userId });
-
-    if (!user) {
-      returnable.push(
-        new ValidationError({
-          field: 'userId',
-          msg: `No user found with id ${userId}`,
-        }),
-      );
-    }
-
-    return returnable;
-  };
+  const getValidationErrors = validator(dependencies);
 
   const execute = async ({ userId, productIds, date, isPayed, meta }) => {
     const order = new Order({
@@ -74,14 +23,16 @@ module.exports = (dependencies) => {
       meta,
     });
 
-    const validationErrors = await getValidationErrors({ order });
+    const validationErrors = await getValidationErrors({
+      order,
+    });
 
     if (!isEmpty(validationErrors)) {
       return Promise.reject(
         new ResponseError({
           status: 403,
-          msg: 'Validation errors',
-          reason: 'Bad data sent',
+          msg: 'Validation Errors',
+          reason: 'Somebody sent bad data',
           validationErrors,
         }),
       );
@@ -90,5 +41,7 @@ module.exports = (dependencies) => {
     return ordersRepository.add(order);
   };
 
-  return { execute };
+  return {
+    execute,
+  };
 };
--- src/useCases/orders/updateOrder.useCase.js
+++ src/useCases/orders/updateOrder.useCase.js
@@ -1,10 +1,31 @@
+const { isEmpty } = require('lodash');
+const { ResponseError } = require('../../frameworks/common');
+const validator = require('./validator');
+
 module.exports = (dependencies) => {
   const { ordersRepository } = dependencies;
   if (!ordersRepository) {
     throw new Error('The orders repository should exist in dependencies');
   }
 
-  const execute = ({ order }) => {
+  const getValidationErrors = validator(dependencies);
+
+  const execute = async ({ order }) => {
+    const validationErrors = await getValidationErrors({
+      order,
+    });
+
+    if (!isEmpty(validationErrors)) {
+      return Promise.reject(
+        new ResponseError({
+          status: 403,
+          msg: 'Validation Errors',
+          reason: 'Somebody sent bad data',
+          validationErrors,
+        }),
+      );
+    }
+
     return ordersRepository.update(order);
   };
 
