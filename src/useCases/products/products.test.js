5c689e34991a5ecea204cfdfe57cba3ab6ed1a59
Willem Verbuyst <willemverbuyst@gmail.com>
2021-09-21 05:04:24 +0200

    Separate test add product usecase

--- src/useCases/products/products.test.js
+++ src/useCases/products/products.test.js
@@ -44,27 +44,6 @@ describe('Product use cases', () => {
     productsRepository: mockProductRepo,
   };
 
-  describe('Add product use case', () => {
-    test('Product shoud be added', async () => {
-      // add product using the use case
-      const addedProduct = await addProductUseCase(depencies).execute(
-        testProductData,
-      );
-      // check the received data
-      expect(addedProduct).toBeDefined();
-      expect(addedProduct.id).toBeDefined();
-      expect(addedProduct.name).toBe(testProductData.name);
-      expect(addedProduct.description).toBe(testProductData.description);
-      expect(addedProduct.images).toEqual(testProductData.images);
-      expect(addedProduct.price).toBe(testProductData.price);
-      expect(addedProduct.color).toBe(testProductData.color);
-      expect(addedProduct.meta).toEqual(testProductData.meta);
-      // check that the depencies are called as expected
-      const expectedProduct = mockProductRepo.add.mock.calls[0][0];
-      expect(expectedProduct).toEqual(testProductData);
-    });
-  });
-
   describe('GetById product use case', () => {
     test('Product should be returned by id', async () => {
       // generate fake id
