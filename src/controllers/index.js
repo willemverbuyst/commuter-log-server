5754a302919eed6b3fe11e49edf415b394d15ca4
Willem Verbuyst <willemverbuyst@gmail.com>
2021-09-22 19:25:07 +0200

    Add update log controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,8 +1,10 @@
+const logControllers = require('./logs');
 const orderControllers = require('./orders');
 const productControllers = require('./products');
 const userControllers = require('./user');
 
 module.exports = {
+  logControllers,
   orderControllers,
   productControllers,
   userControllers,
